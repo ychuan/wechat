51dd0d086d9e003f1b0809386d7c8df68059b65b
ychuan@ghan.com.cn <love_java@live.cn>
2019-07-17 00:13:02 +0800

    Update pay.go
    change position

--- pay/pay.go
+++ pay/pay.go
@@ -95,12 +95,13 @@ func (pcf *Pay) PrePayOrder(p *Params) (payOrder PreOrder, err error) {
 	param["body"] = p.Body
 	param["mch_id"] = pcf.PayMchID
 	param["nonce_str"] =nonceStr
+	param["notify_url"] = p.NotifyUrl
 	param["out_trade_no"] =p.OutTradeNo
 	param["spbill_create_ip"] =p.CreateIP
 	param["total_fee"] =p.TotalFee
 	param["trade_type"] =p.TradeType
 	param["openid"] = p.OpenID
-	param["notify_url"] = p.NotifyUrl
+
 
 	bizKey := "&key="+pcf.PayKey
 	str := orderParam(param,bizKey)
